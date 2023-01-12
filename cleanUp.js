8eed3d1773b773f5870034be58d092005a979746
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-01-12 14:25:21 -0800

    Adds two files

--- cleanUp.js
+++ cleanUp.js
@@ -9,38 +9,43 @@
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+nameQuestion();
+// For example 1 I've added a ; after the parentheseis of the console.log and after the call parenthesis becasuse it's best practice. 
+
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
+//For example 2 I have indented line 22 and 23 and brought the last curly bracket inline under function so it's easy to read/see the code block. 
 
 
-// EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
 
+//For example 3 I've completed the word function that wasn't typed fully. 
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
-
-  console.log(`the average is: ${avg}`.)
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  console.log(`the average is: ${avg}`)
   }
+
+average(4, 5);
+//For example 4 I've brought the curly bracket back up to line 44. Indented line 45. Took away space on line 46. Deleted the . before the last parenthesis on the console.log. I've also called the funtion with 4 and 5 in order to get it to carry out its instructions.
